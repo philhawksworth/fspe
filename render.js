eb075999a5b09b0a6fc8ae9d7a5767d38bb576a7
Phil Hawksworth <phawksworth@gmail.com>
2016-03-06 18:59:04 +0000

    simple data resolution and templates

---
+++ render.js
@@ -0,0 +1,56 @@
+function addEventHandlers() {
+
+  var dynamicPageLinks = document.querySelectorAll('[data-template]');
+
+  $(dynamicPageLinks).click(function(e){
+    e.preventDefault();
+    var dataSource = returnAPIPath(e.target.pathname);
+    var template = e.target.getAttribute("data-template");
+    $.get(dataSource, function( data ) {
+      renderContent(template, data);
+      setAddress(e.target.pathname);
+    }, "json" );
+  });
+
+};
+
+function setAddress(path){
+  var stateObj = { foo: "bar" };
+  history.pushState(stateObj, null, path);
+}
+
+
+// Determine the api path from the hijcked link's href
+function returnAPIPath(path){
+  return "/api" + path + ".json";
+}
+
+
+// Output some data to the page via a given template
+function renderContent(template, data){
+
+  //source the hbs template
+  var hbsTemplate = $("#"+template).html();
+
+  // Compile the template
+  var compiledTemplate = Handlebars.compile(hbsTemplate);
+
+  // populate the template with data
+  var output = compiledTemplate(data);
+
+  // add output to the page
+  $('.content').html(output);
+
+};
+
+
+
+$(function () {
+
+
+  addEventHandlers();
+
+
+});
+
+
