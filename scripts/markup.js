d3ebc9e55dfeda6acc6d4fb91f7adcca4f505c5c
Phil Hawksworth <phawksworth@gmail.com>
2016-03-13 20:46:51 +0000

    dotjs templates'

--- scripts/markup.js
+++ scripts/markup.js
@@ -19,66 +19,29 @@ handlebars.registerPartial('base', fs.readFileSync( paths.layouts + 'base.hbs', 
 handlebars.registerPartial('page', fs.readFileSync( paths.layouts + '../page-template.hbs', 'utf8'));
 handlebars.registerPartial('navigation', fs.readFileSync( paths.partials + 'navigation.hbs', 'utf8'));
 
+require.extensions['.hbs'] = function (module, filename) {
+  var file = fs.readFileSync(filename, "utf8");
+  var opts = { traverse: true };
+  return module._compile(hbsfy.compile(file, opts), filename);
+};
+
 // set up the dist folder
 utils.ensureFolder(paths.output);
 
 
 /*
- generate the static html pages found in the Pages directory
-*/
-function generatePages() {
-
-  glob("**/*.hbs", {cwd: paths.pages}, function (er, files) {
-
-    for (var i = 0; i < files.length; i++) {
-
-      // get the data from the corresponding api source
-      var api = paths.api + files[i].replace(".hbs",".json");
-      var data = JSON.parse(fs.readFileSync(api, 'utf8'));
-
-      // render the data into the template
-      var hbsTemplate = fs.readFileSync(paths.pages + files[i], 'utf8');
-      var template = handlebars.compile(hbsTemplate);
-      var result = template(data);
-
-      // ensure that recursive filders exist
-      var filePath = files[i].match(/(.*)\/(.*)/);
-      if(filePath) {
-        utils.ensureFolder(paths.output + filePath[1]);
-      }
-
-      // output the result to file
-      var outputDest = paths.output + files[i].replace(".hbs",".html");
-      var writeStream = fs.createWriteStream(outputDest);
-      writeStream.write(result);
-      writeStream.end();
-
-      console.log(
-        chalk.grey("  compiled"),
-        outputDest
-      );
-
-    }
-  });
-
-}
-
-
-/*
- generate the static html pages found in the Pages directory
+ generate the static html pages for items found in the API sitemap
 */
 function generatePagesFromApi() {
 
 
   for(var p in pages){
-    // console.log(chalk.red(p), pages[p]);
-
 
     // choose template
     var templateFile = pages[p].template + ".hbs";
 
     // get the data from the corresponding api source
-    var api = paths.api + p + ".json";
+    var api = paths.api + p + ".json"
     var data = JSON.parse(fs.readFileSync(api, 'utf8'));
 
 
@@ -103,44 +66,14 @@ function generatePagesFromApi() {
     );
   }
 
-
-
-
-  // glob("**/*.hbs", {cwd: paths.pages}, function (er, files) {
-
-  //   for (var i = 0; i < files.length; i++) {
-
-  //     // get the data from the corresponding api source
-  //     var api = paths.api + files[i].replace(".hbs",".json");
-  //     var data = JSON.parse(fs.readFileSync(api, 'utf8'));
-
-  //     // render the data into the template
-  //     var hbsTemplate = fs.readFileSync(paths.pages + files[i], 'utf8');
-  //     var template = handlebars.compile(hbsTemplate);
-  //     var result = template(data);
-
-
-  //     // output the result to file
-  //     var outputDest = paths.output + files[i].replace(".hbs",".html");
-  //     var writeStream = fs.createWriteStream(outputDest);
-  //     writeStream.write(result);
-  //     writeStream.end();
-
-
-  //   }
-  // });
-
 }
+
+
 
 generatePagesFromApi();
 
 
 
 
-require.extensions['.hbs'] = function (module, filename) {
-  var file = fs.readFileSync(filename, "utf8");
-  var opts = { traverse: true };
-  return module._compile(hbsfy.compile(file, opts), filename);
-};
 
 
